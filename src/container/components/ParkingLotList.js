a3be844af3cc061e537c52ed0eb31acbebfaecf3
jungwoo <cyjjw2@gmail.com>
2024-09-08 03:00:57 +0900

    ccc

--- src/container/components/ParkingLotList.js
+++ src/container/components/ParkingLotList.js
@@ -1,8 +1,8 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+// ... existing code ...
 const ParkingLotList = ({ parkingLots }) => {
   return (
-    <div style={{ width: '300px', height: '100vh', overflowY: 'scroll', position: 'absolute', left: 0, top: 0, backgroundColor: 'white', zIndex: 10, padding: '10px' }}>
+    <div style={{ width: '300px', height: 'calc(100vh - 200px)', overflowY: 'scroll', position: 'absolute', left: 0, top: '180px', backgroundColor: 'white', zIndex: 10, padding: '10px' }}>
       <h2>주차장 리스트</h2>
       <ul>
         {parkingLots.map((lot, index) => (
